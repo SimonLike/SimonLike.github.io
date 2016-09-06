0e155d24b2ad9bdea69f5ef34180ca457579f6c9
simon <simon@simondeiMac.local>
2016-09-06 16:41:54 +0800

    首页大变动

---
+++ backstage/sidebar.php
@@ -0,0 +1,54 @@
+{
+    "data": [
+    {
+        "blogid": "10007",
+        "type": "1",
+        "conent": "JavaScriptCore框架",
+        "time":"2016-09-01",
+        "url": "JavaScriptCore"
+    },
+    {
+        "blogid": "10006",
+        "type": "2",
+        "conent": "Mac OS下fis3 安装教程",
+        "time":"2016-06-28",
+        "url": "Mac_OS_Fis3"
+    },
+    {
+        "blogid": "10005",
+        "type": "1",
+        "conent": "delegate,block,notification三者的运用",
+        "time":"2016-06-23",
+        "url": "delegate_block_notification"
+    },
+     {
+        "blogid": "10004",
+        "type": "1",
+        "conent": "IOS常用的宏定义",
+        "time":"2016-06-22",
+        "url": "IOS_hong"
+    },
+    {
+        "blogid": "10003",
+        "type": "1",
+        "conent": "动态获取图片色值，黑点坐标",
+        "time":"2016-06-15",
+        "url": "color_location"
+    },
+    {
+        "blogid": "10002",
+        "type": "1",
+        "conent": "多个button，改变选中button的背景颜色、字体颜色",
+        "time":"2016-06-14",
+        "url": "moreButton_Color"
+    },
+    {
+        "blogid": "10001",
+        "type": "1",
+        "conent": "同一个tableview实现多选单选删除",
+        "time":"2016-06-13",
+        "url": "tableview_select"
+    }
+   
+    ]
+}
---
+++ js/index.js
@@ -0,0 +1,106 @@
+
+var dataInfo = [];//菜单值
+
+//初始化
+$(document).ready(function(){
+    // console.log(666);
+});
+
+angular.module('myapp',['ngRoute'])
+/*
+ *平级controller传值处理
+ *@param $on
+ *@param $broadcast
+ */
+.run(function($rootScope){
+    $rootScope.$on('dataEmit',function(event,data){//接收传值
+        $rootScope.$broadcast('dataBroadcast',data);//发布传值
+    });
+
+    $rootScope.$on('dataDetailEmit',function(event,data){//接收传值
+        $rootScope.$broadcast('dataDetailBroadcast',data);//发布传值
+    });
+})
+/*
+ * 左边模块 iOS html
+ */
+.controller('leftctl', function($scope) {
+    $scope.leftinfo = [{name:'所有', index:0},{name:'iOS', index:1},{name:'HTML', index:2}];
+    setTimeout(function() {
+        $('#side0').addClass('btn_active');//元素的选中状态
+    }, 60);
+    $scope.sideDeatil = function (code) {
+        $('#side'+code).siblings().removeClass('btn_active');//移除同级元素的选中状态
+        $('#side'+code).addClass('btn_active');//元素的选中状态
+        $scope.$emit('dataEmit',$scope.leftinfo[code]);//传值其他controller
+    };
+})
+ /*
+ * 菜单列表
+ */
+.controller('sidebarctl', function($scope, $http) {
+    $http.get("backstage/sidebar.php")//读取本地数据
+    .success(function (response) {
+        $scope.names = response.data;
+        dataInfo = $scope.names ;
+        console.log($scope.names);
+    });
+    //菜单列表点击事件
+    $scope.joinDetail = function(index){
+        console.log($scope.names[index- 1]);
+        $scope.$emit('dataDetailEmit',$scope.names[index-1].url);//传值其他controller
+    }
+     
+    $scope.$on('dataBroadcast',function(event,data){//接收其他controller传值
+        $('#menu_title').text(data.name);
+        if (data.index !=0) {
+            var arr = new Array();
+            for (var i = 0; i < dataInfo.length; i++) {
+                if (dataInfo[i].type == data.index) {
+                    arr.push(dataInfo[i]);
+                }
+            }  
+            $scope.names = arr;
+        }else{
+            $scope.names = dataInfo;
+        }
+    })
+})
+/*
+ * 内容显示
+ */
+.controller('contentctl', function ($scope, $route) { $scope.$route = $route;})
+/*
+ * 路由点击事件
+ */
+.config(['$routeProvider', function($routeProvider){
+
+    $routeProvider
+    
+    .when('/JavaScriptCore',{
+        templateUrl:'html/JavaScriptCore.html'
+    })
+    .when('/Mac_OS_Fis3',{
+        templateUrl:'html/Mac_OS_Fis3.html'
+    })
+    .when('/delegate_block_notification',{
+        templateUrl:'html/delegate_block_notification.html'
+    })
+    .when('/IOS_hong',{
+        templateUrl:'html/IOS_hong.html'
+    })
+    .when('/color_location',{
+        templateUrl:'html/color_location.html'
+    })
+    .when('/moreButton_Color',{
+        templateUrl:'html/moreButton_Color.html'
+    })
+    .when('/tableview_select',{
+        templateUrl:'html/tableview_select.html'
+    })
+    .otherwise({
+        redirectTo:'/'
+
+    });
+}]) ;
+
